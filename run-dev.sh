40429093b0535ba499dd37869c488f26a7304a2a
tauasa <tauasa@gmail.com>
2026-07-17 17:28:43 -0700

    Script update

--- run-dev.sh
+++ run-dev.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 
-# launch from the project's target directory in development mode
+# launch from the project's target directory in dev mode
 
-# if the JUDI_PATH environment variable is set, use it to launch the jar file
-if [ -n "$JUDI_PATH" ]; then
-    echo "Launching judi-http from $JUDI_PATH"
-    java -jar "$JUDI_PATH/judi-http.jar"
-else
-# otherwise, launch from the target directory
-    echo "Launching judi-http from target directory"
-    java -jar target/judi-http.jar
-fi
+echo "Launching judi-http from target directory"
+java -jar target/judi-http.jar
